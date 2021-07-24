f421de3f5a29eb4e06ff710608eaf134ad5704e1
Dharmik Maru <dhharmikmaru76@gmail.com>
2021-07-24 17:09:54 +0530

    Hello servlet

---
+++ HelloWebApp/src/main/java/com/pheonix/web/GreetingServlet.java
@@ -0,0 +1,46 @@
+package com.pheonix.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GreetingServlet
+ */
+public class GreetingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("Init Method");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("Destroy Method");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Service Method Start");
+		PrintWriter out = response.getWriter();
+		out.println("<p>Welcome to Dynamic Web Application</p>");
+		
+		
+	}
+
+}
